2f43c1279640119b1ce79a3c660b71b67262f425
chadnovgyst <chadnov@gmail.com>
2025-11-07 12:53:24 -0800

    release: add base-prefixed assets

---
+++ _app/immutable/chunks/BxtykZZu.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1g3boqa?.base??"/nerdsreunion2026_phase2",a=globalThis.__sveltekit_1g3boqa?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.BflkF5Jx.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CV1dlngQ.js";export{o as load_css,r as start};
