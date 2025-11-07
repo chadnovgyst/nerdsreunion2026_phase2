53dc9bbe5096a7a28a91cff2276a20b72b4a8694
chadnovgyst <chadnov@gmail.com>
2025-11-07 13:02:46 -0800

    release: update tew copy

--- _app/immutable/chunks/BxtykZZu.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1g3boqa?.base??"/nerdsreunion2026_phase2",a=globalThis.__sveltekit_1g3boqa?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/CsVXtgB5.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_xba5gb?.base??"/nerdsreunion2026_phase2",a=globalThis.__sveltekit_xba5gb?.assets??s??"";export{a,s as b};
