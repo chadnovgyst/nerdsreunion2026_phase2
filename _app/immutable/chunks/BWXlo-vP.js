8f01454969865b8664eb4c6bd6cd4b8716524a29
chadnovgyst <chadnov@gmail.com>
2025-11-10 00:28:41 -0800

    release: intro fallback + shuffle cleanup

---
+++ _app/immutable/chunks/BWXlo-vP.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_a4614p?.base??"/nerdsreunion2026_phase2",a=globalThis.__sveltekit_a4614p?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/Dh8RSm7Z.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_152tst7?.base??"/nerdsreunion2026_phase2",t=globalThis.__sveltekit_152tst7?.assets??s??"";export{t as a,s as b};
